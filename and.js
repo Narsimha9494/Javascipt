5ca616adbb91834365719589754ec6cec8f82180
Narsimha9494 <nrk11998@gmail.com>
2023-06-02 16:24:53 -0500

    day2

---
+++ and.js
@@ -0,0 +1,38 @@
+const prompt = require('prompt-sync')();
+
+const gender = prompt('What is your gender? :');
+console.log(" ");
+
+const something = false;
+
+if(gender == 'male') {
+    const valtype = prompt('Please enter your sal :');
+let sal = valtype
+if (sal >= 6000 && sal < 12000){
+console.log(' Good boy');
+}
+else if (sal >= 12000 && sal < 18000){
+console.log('very good boy');
+}
+else
+console.log('out of range');
+}
+
+
+else if(gender == 'female') {
+    const valtype = prompt('Please enter your sal :');
+let sal = valtype
+if (sal >= 6000 && sal < 12000){
+console.log(' Good girl');
+}
+else if (sal >= 12000 && sal < 18000){
+console.log('very good girl');
+}
+else
+console.log('out of range');
+}
+
+else
+    console.log("Invalid Gender\n");
+
+
