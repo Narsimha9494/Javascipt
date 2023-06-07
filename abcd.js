130d4e0c658432296ae97677bbd9d9aa4dc6b11e
Narsimha9494 <nrk11998@gmail.com>
2023-06-06 19:24:11 -0500

    Day3

---
+++ abcd.js
@@ -0,0 +1,9 @@
+let firstName = "hey";
+let age = 21;
+
+console.log(firstName);
+console.log(age);
+document.getElementById("p1").innerHTML = "red" + "  "  +  firstName;
+let userName = window.prompt("hello how are you?");
+console.log(userName);
+
